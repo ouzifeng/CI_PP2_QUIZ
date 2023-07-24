ac81379c86c0b8eaf01e9b75d32816eae66281b9
David Oak <mr.davidoak@gmail.com>
2023-07-24 12:04:05 +0100

    create beer title variable

--- assets/js/randomBeer.js
+++ assets/js/randomBeer.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const randomMyBeer = document.querySelector(".random-beer");
     const descriptionDisplay = document.querySelector(".beer-description")
+    const titleDisplay = document.querySelector(".beer-title")
 });
