3e99aafbe4cd4abd6ced692080a8c66dbd217752
David Oak <mr.davidoak@gmail.com>
2023-07-28 14:45:54 +0100

    add questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,21 +1,202 @@
 // Create questions
 const questions = [
   {
-    question: 'abcd',
+    question: 'What is craft beer?',
     answers: [
-      { text: 'shark', isCorrect: false },
-      { text: 'shark', isCorrect: false },
-      { text: 'shark', isCorrect: false },
-      { text: 'not shark', isCorrect: true }
+      { text: 'A beer made in a factory', isCorrect: false },
+      { text: 'Any beer that is not a lager', isCorrect: false },
+      {
+        text: 'A beer brewed in a traditional or non-mechanized way by a small brewery',
+        isCorrect: true
+      },
+      { text: 'A beer that contains crafts', isCorrect: false }
     ]
   },
   {
-    question: 'abcd1',
+    question: 'Which country has the most craft breweries per capita?',
     answers: [
-      { text: 'shark', isCorrect: false },
-      { text: 'shark', isCorrect: false },
-      { text: 'shark', isCorrect: false },
-      { text: 'not shark', isCorrect: true }
+      { text: 'Germany', isCorrect: false },
+      { text: 'United States', isCorrect: false },
+      { text: 'Belgium', isCorrect: false },
+      { text: 'United Kingdom', isCorrect: true }
+    ]
+  },
+  {
+    question: 'What is the main ingredient in beer?',
+    answers: [
+      { text: 'Hops', isCorrect: false },
+      { text: 'Barley', isCorrect: true },
+      { text: 'Yeast', isCorrect: false },
+      { text: 'Water', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What is a "session" beer?',
+    answers: [
+      {
+        text: 'A beer that is brewed for a special occasion',
+        isCorrect: false
+      },
+      { text: 'A beer that has a low alcohol content', isCorrect: true },
+      {
+        text: 'A beer that is brewed during a particular season',
+        isCorrect: false
+      },
+      { text: 'A beer that is used for tasting sessions', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What does IPA stand for?',
+    answers: [
+      { text: 'Intensely Potent Ale', isCorrect: false },
+      { text: 'International Pale Ale', isCorrect: false },
+      { text: 'Indian Pale Ale', isCorrect: true },
+      { text: 'Instantly Perfect Ale', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What is the process of making beer called?',
+    answers: [
+      { text: 'Brewing', isCorrect: true },
+      { text: 'Fermenting', isCorrect: false },
+      { text: 'Distilling', isCorrect: false },
+      { text: 'Vinting', isCorrect: false }
+    ]
+  },
+  {
+    question: 'Which ingredient adds bitterness to beer?',
+    answers: [
+      { text: 'Hops', isCorrect: true },
+      { text: 'Yeast', isCorrect: false },
+      { text: 'Barley', isCorrect: false },
+      { text: 'Water', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What type of beer is typically dark and sweet?',
+    answers: [
+      { text: 'Pilsner', isCorrect: false },
+      { text: 'Stout', isCorrect: true },
+      { text: 'IPA', isCorrect: false },
+      { text: 'Sour', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What is a lager?',
+    answers: [
+      {
+        text: 'A beer that is brewed with bottom-fermenting yeast',
+        isCorrect: true
+      },
+      {
+        text: 'A beer that is brewed with top-fermenting yeast',
+        isCorrect: false
+      },
+      { text: 'A beer that is brewed with wild yeast', isCorrect: false },
+      { text: 'A beer that is brewed without yeast', isCorrect: false }
+    ]
+  },
+  {
+    question:
+      'What is the name of the Belgian beer that is fermented in the bottle?',
+    answers: [
+      { text: 'Gueuze', isCorrect: false },
+      { text: 'Trappist', isCorrect: false },
+      { text: 'Lambic', isCorrect: false },
+      { text: 'Champagne Beer', isCorrect: true }
+    ]
+  },
+  {
+    question: 'What does ABV stand for in the context of beer?',
+    answers: [
+      { text: 'Average Barley Volume', isCorrect: false },
+      { text: 'Alcohol By Volume', isCorrect: true },
+      { text: 'Advanced Brewing Variable', isCorrect: false },
+      { text: 'Alternative Beer Variety', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What is a "dry-hopped" beer?',
+    answers: [
+      {
+        text: 'A beer that has had hops added during fermentation',
+        isCorrect: true
+      },
+      {
+        text: 'A beer that has been brewed without any water',
+        isCorrect: false
+      },
+      {
+        text: 'A beer that has had hops removed after brewing',
+        isCorrect: false
+      },
+      {
+        text: 'A beer that has not been brewed with any hops',
+        isCorrect: false
+      }
+    ]
+  },
+  {
+    question: 'Which beer style is known for its banana and clove flavors?',
+    answers: [
+      { text: 'American Pale Ale', isCorrect: false },
+      { text: 'Irish Stout', isCorrect: false },
+      { text: 'German Hefeweizen', isCorrect: true },
+      { text: 'English Bitter', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What does it mean when a beer is "skunked"?',
+    answers: [
+      { text: 'It has gone bad due to light exposure', isCorrect: true },
+      { text: 'It has a high alcohol content', isCorrect: false },
+      { text: 'It has been brewed with skunk hops', isCorrect: false },
+      { text: 'It has a unique, musky flavor', isCorrect: false }
+    ]
+  }
+{
+    question: 'Which country is the original producer of Pilsner?',
+    answers: [
+      { text: 'Germany', isCorrect: false },
+      { text: 'United States', isCorrect: false },
+      { text: 'Belgium', isCorrect: false },
+      { text: 'Czech Republic', isCorrect: true }
+    ]
+  },
+  {
+    question: 'What is the world’s strongest beer by ABV?',
+    answers: [
+      { text: 'BrewDog Tactical Nuclear Penguin', isCorrect: false },
+      { text: 'Samuel Adams Utopias', isCorrect: false },
+      { text: 'Snake Venom by Brewmeister', isCorrect: true },
+      { text: 'The End of History by BrewDog', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What is the purpose of yeast in beer brewing?',
+    answers: [
+      { text: 'It adds bitterness to the beer', isCorrect: false },
+      { text: 'It adds color to the beer', isCorrect: false },
+      { text: 'It ferments the sugars, producing alcohol and carbon dioxide', isCorrect: true },
+      { text: 'It sterilizes the beer', isCorrect: false }
+    ]
+  },
+  {
+    question: 'What is the Reinheitsgebot?',
+    answers: [
+      { text: 'A famous beer festival in Germany', isCorrect: false },
+      { text: 'A type of German lager', isCorrect: false },
+      { text: 'The German Beer Purity Law', isCorrect: true },
+      { text: 'A brewing technique', isCorrect: false }
+    ]
+  },
+  {
+    question: 'Which fruit is commonly used in a lambic beer?',
+    answers: [
+      { text: 'Apple', isCorrect: false },
+      { text: 'Cherry', isCorrect: true },
+      { text: 'Orange', isCorrect: false },
+      { text: 'Lemon', isCorrect: false }
     ]
   }
 ]
