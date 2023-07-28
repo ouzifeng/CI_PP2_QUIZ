c7f850464c9b44e36fb80fa690d3322b455fc239
David Oak <mr.davidoak@gmail.com>
2023-07-28 10:52:32 +0100

    integrated emailjs to send score as email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,20 +1,23 @@
+emailjs.init('-KnEtVWAz0V-IAlEz') // Initialize EmailJS with your user ID
+
 document
   .getElementById('resultForm')
   .addEventListener('submit', function (event) {
+    console.log('Form submitted') // Add this line
     event.preventDefault()
-
-    emailjs
-      .send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', {
-        from_name: document.getElementById('name').value,
-        from_email: document.getElementById('email').value,
-        message: 'Your score: ' + score
-      })
-      .then(
-        function () {
-          console.log('SUCCESS!')
-        },
-        function (error) {
-          console.log('FAILED...', error)
-        }
-      )
   })
+
+emailjs
+  .send('service_7cwu2tw', 'template_cyk3kf9', {
+    from_name: document.getElementById('name').value,
+    from_email: document.getElementById('email').value,
+    message: 'Your score: ' + score
+  })
+  .then(
+    function () {
+      console.log('SUCCESS!')
+    },
+    function (error) {
+      console.log('FAILED...', error)
+    }
+  )
