95e4bd3ec2122bf308e48ef94454f74a8ebe039b
David Oak <mr.davidoak@gmail.com>
2023-07-28 14:38:34 +0100

    improve responsive design on mobile for index page

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,21 +3,23 @@ emailjs.init('-KnEtVWAz0V-IAlEz') // Initialize EmailJS with your user ID
 document
   .getElementById('resultForm')
   .addEventListener('submit', function (event) {
-    console.log('Form submitted') // Add this line
     event.preventDefault()
-  })
 
-emailjs
-  .send('service_7cwu2tw', 'template_cyk3kf9', {
-    from_name: document.getElementById('name').value,
-    from_email: document.getElementById('email').value,
-    message: 'Your score: ' + score + ' out' + ' of' + ' 10'
+    emailjs
+      .send('service_7cwu2tw', 'template_cyk3kf9', {
+        from_name: document.getElementById('name').value,
+        from_email: document.getElementById('email').value,
+        message: 'Your score: ' + score
+      })
+      .then(
+        function () {
+          // After the email is sent, hide the form and show the "thanks" message
+          document.getElementById('resultForm').style.display = 'none'
+          document.getElementById('thanks').style.display = 'block'
+          console.log('SUCCESS!')
+        },
+        function (error) {
+          console.log('FAILED...', error)
+        }
+      )
   })
-  .then(
-    function () {
-      console.log('SUCCESS!')
-    },
-    function (error) {
-      console.log('FAILED...', error)
-    }
-  )
