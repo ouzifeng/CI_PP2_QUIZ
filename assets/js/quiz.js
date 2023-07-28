e328c2e3966b6380b13d4a57ac5318d51786e23e
David Oak <mr.davidoak@gmail.com>
2023-07-28 09:32:56 +0100

    create test questions

---
+++ assets/js/quiz.js
@@ -0,0 +1,21 @@
+// Create questions
+const questions = [
+  {
+    question: 'abcd',
+    answers: [
+      { text: 'shark', isCorrect: false },
+      { text: 'shark', isCorrect: false },
+      { text: 'shark', isCorrect: false },
+      { text: 'not shark', isCorrect: true }
+    ]
+  },
+  {
+    question: 'abcd1',
+    answers: [
+      { text: 'shark', isCorrect: false },
+      { text: 'shark', isCorrect: false },
+      { text: 'shark', isCorrect: false },
+      { text: 'not shark', isCorrect: true }
+    ]
+  }
+]
